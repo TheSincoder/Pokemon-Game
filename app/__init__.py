4c27b47c81eb6d7cd4dc4380f5254ab08e67a428
NobleSincoder <sincode.reeves@gmail.com>
2022-02-02 21:15:48 -0700

    Login, Register, Logout

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,24 @@
+# Initializing things
 from flask import Flask
 from config import Config
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+# init the app
 app = Flask(__name__)
+
+# link our config to our app
 app.config.from_object(Config)
 
-from app import routes
+# init my Login Manager
+login= LoginManager(app)
+# send here when not logged in if trying to access login page
+login.login_view='login'
+
+# do inits for database stuff
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+
+from app import routes, models
